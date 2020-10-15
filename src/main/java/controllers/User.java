6b950f7934f69bb0bfdd532a1f14033081364e97
OliverEHConway <91860@farnborough.ac.uk>
2020-10-15 12:02:07 +0100

    Added Login page and working login

--- src/main/java/controllers/User.java
+++ src/main/java/controllers/User.java
@@ -1,16 +1,15 @@
 package controllers;
 
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import server.Main;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.UUID;
 
 @Path("user")
 @Consumes(MediaType.MULTIPART_FORM_DATA)
@@ -38,6 +37,48 @@ public class User {
         } catch (Exception exception) {
             System.out.println("Database error: " + exception.getMessage());
             return "{\"Error\": \"Unable to list items.  Error code 1.\"}";
+        }
+    }
+    @POST
+    @Path("login")
+    public String loginUser(@FormDataParam("username") String username, @FormDataParam("password") String password) {
+        System.out.println("Invoked loginUser() on path user/login");
+        try {
+            PreparedStatement ps1 = Main.db.prepareStatement("SELECT Password FROM Users WHERE Username = ?");
+            ps1.setString(1, username);
+            ResultSet loginResults = ps1.executeQuery();
+            if (loginResults.next() == true) {
+                String correctPassword = loginResults.getString(1);
+                if (password.equals(correctPassword)) {
+                    String token = UUID.randomUUID().toString();
+                    PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = ? WHERE Username = ?");
+                    ps2.setString(1,token);
+                    ps2.setString(2,username);
+                    ps2.executeUpdate();
+                    JSONObject userDetails = new JSONObject();
+                    userDetails.put("username", username);
+                    userDetails.put("token", token);
+                    return userDetails.toString();
+                } else {
+                    return "{\"Error\": \"Username and password are incorrect.\"}";
+                }
+            } else {
+                return "{\"Error\": \"Username and password are incorrect.\"}";
+            }
+        }catch (Exception exception) {
+            System.out.println("Database error during /user/login: " + exception.getMessage());
+            return "{\"Error\": \"Server side error!\"}";
+        }
+    }
+    public static boolean validToken(String token) {
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT UserID FROM Users WHERE Token = ?");
+            ps.setString(1,token);
+            ResultSet logoutResults = ps.executeQuery();
+            return logoutResults.next();
+        } catch (Exception exception) {
+            System.out.println("Database error" + exception.getMessage());
+            return false;
         }
     }
 }
